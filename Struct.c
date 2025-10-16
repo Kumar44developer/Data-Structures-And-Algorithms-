21c9f817a01cb8fffb2540ba246d752180e5cbb2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-16 16:24:27 +0530

    Implementation of the  struct named planner and Learnt Dynamic Memory Allocation
    Added a struct to represent a planner and a function to create an array of planners with dynamic memory allocation.

--- Struct.c
+++ Struct.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct planner {
+    char day[10];
+    int date;
+    char activity[50];
+};
+
+struct planner* create(int n) {
+    struct planner* p;
+    p = (struct planner*)malloc(n * sizeof(struct planner));
+    if (p == NULL) {
+        printf("Memory Allocation Failed\n");
+        exit(0);
+    }
+    return p;
+}
