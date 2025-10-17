6253e4ca341626793ff360ad9c516eef879ceb9f
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-17 07:50:14 +0530

    Added read function for user input
    Added a read function to take user input for string, pattern, and replacement.

--- struct2.c
+++ struct2.c
@@ -2,7 +2,14 @@
 #include <string.h>
 
 char str[100], pat[100], rep[100], ans[200];
-
+void read() {
+    printf("Enter the string: ");
+    gets(str);  // Note: gets() is unsafe, consider fgets() instead
+    printf("Enter the pattern string: ");
+    gets(pat);
+    printf("Enter the replacement string: ");
+    gets(rep);
+}
 
 
 
