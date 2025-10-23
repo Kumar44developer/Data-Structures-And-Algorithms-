17aa7c5e870b79a0ee78b799c578f592b153ac93
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-23 13:56:08 +0530

    Added the function named evaluatePostfix function and performed the respective operations in it

--- stacks2.c
+++ stacks2.c
@@ -14,4 +14,23 @@ int pop() {
     return stack[top--];
 }
 
+int evaluatePostfix(char *exp) {
+    for (int i = 0; exp[i] != '\0'; i++) {
+        char c = exp[i];
+
+        if (isdigit(c)) {
+            push(c - '0');
+        } else {
+            int val2 = pop();
+            int val1 = pop();
+            switch (c) {
+                case '+': push(val1 + val2); break;
+                case '-': push(val1 - val2); break;
+                case '*': push(val1 * val2); break;
+                case '/': push(val1 / val2); break;
+            }
+        }
+    }
+    return pop();
+}
 
