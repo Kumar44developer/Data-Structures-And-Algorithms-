844c05d6db4aed8c715fbc2946436e28266b3b05
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-23 11:25:38 +0530

    Updated stacks.c by adding the precedence function to it (which takes the char input)

--- stacks.c
+++ stacks.c
@@ -14,5 +14,16 @@ void push(char c) {
 char pop() {
     return stack[top--];
 }
+int precedence(char c) {
+    if (c == '^')
+        return 3;
+    else if (c == '*' || c == '/')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+
 
 
