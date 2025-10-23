eaf26b0ef55e3dad6cf22668b9d61ee58f7c4d17
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-23 11:27:05 +0530

    Updated stacks.c by Adding the infixtopostfix conversion program in C programming file

--- stacks.c
+++ stacks.c
@@ -25,5 +25,29 @@ int precedence(char c) {
         return -1;
 }
 
+void infixToPostfix(char *exp) {
+    char result[MAX];
+    int k = 0;
+    for (int i = 0; exp[i] != '\0'; i++) {
+        char c = exp[i];
 
+        if (isalnum(c)) {
+            result[k++] = c;
+        }
+        else if (c == '(') {
+            push(c);
+        }
+        else if (c == ')') {
+            while (top != -1 && stack[top] != '(')
+                result[k++] = pop();
+            pop(); // pop '('
+        }
+        else {
+            while (top != -1 && precedence(stack[top]) >= precedence(c))
+                result[k++] = pop();
+            push(c);
+        }
+    }
+
+  
 
