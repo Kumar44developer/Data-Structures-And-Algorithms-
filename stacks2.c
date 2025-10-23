252246467943d58a251d927456119a02c75b1690
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-23 13:54:50 +0530

    Updated stacks2.c by Adding the push and the pop function

--- stacks2.c
+++ stacks2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int x) {
+    stack[++top] = x;
+}
+
+int pop() {
+    return stack[top--];
+}
+
 
