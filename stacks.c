cd84efa9424a57feae90169e3f6d482122274447
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-23 11:24:34 +0530

    Update stacks.c with the push and the pop operations

--- stacks.c
+++ stacks.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
 
