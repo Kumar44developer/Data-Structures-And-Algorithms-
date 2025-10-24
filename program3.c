125639dd6eb0f0b7a097d61cf8f033bc3a805e3d
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-24 23:06:29 +0530

    Checking whether the given postfix expression is Valid or Not (Writing the function in the C program)

--- program3.c
+++ program3.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int isValidPostfix(char *exp) {
+    int count = 0;
+    for (int i = 0; exp[i] != '\0'; i++) {
+        if (isalnum(exp[i]))
+            count++;
+        else
+            count -= 1;
+        if (count < 1)
+            return 0;
+    }
+    return count == 1;
+}
+
+
 
