7a8af7aac5a9f73d891a4c8874b7723d6477fa9c
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-24 23:11:39 +0530

    Updated program4.c by writing the evaluate postfix expression function to it

--- program4.c
+++ program4.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int x) {
+    stack[++top] = x;
+}
+
+int pop() {
+    return stack[top--];
+}
+
+int evaluatePostfix(char *exp) {
+    for (int i = 0; exp[i] != '\0'; i++) {
+        char c = exp[i];
+
+        if (isdigit(c)) {
+            push(c - '0');
+        } else {
+            int val2 = pop();
+            int val1 = pop();
+            switch (c) {
+                case '+': push(val1 + val2); break;
+                case '-': push(val1 - val2); break;
+                case '*': push(val1 * val2); break;
+                case '/': push(val1 / val2); break;
+            }
+        }
+    }
+    return pop();
+}
 
