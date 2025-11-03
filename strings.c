2d6af99b6a7614f94c9ebbfaaeb4d177f4529065
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-03 09:10:53 +0530

    Added input function to read main string, pattern, and replacement

--- strings.c
+++ strings.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+char str[100], pat[100], rep[100], ans[200];
+
+void read() {
+    printf("Enter the string: ");
+    gets(str);  
+    printf("Enter the pattern string: ");
+    gets(pat);
+    printf("Enter the replacement string: ");
+    gets(rep);
+}
 
