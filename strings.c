c0c316002b08367b9db4ec0faf0e7756de32e1f1
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-03 09:12:47 +0530

    Added variable initialization and length calculation in pat_match function

--- strings.c
+++ strings.c
@@ -11,4 +11,8 @@ void read() {
     printf("Enter the replacement string: ");
     gets(rep);
 }
-
+void pat_match() {
+    int i = 0, j = 0, c = 0, m = 0, k = 0;
+    int flag = 0;
+    int len_str = strlen(str);
+    int len_pat = strlen(pat);
